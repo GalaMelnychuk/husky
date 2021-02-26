21d71bbc5159692888d4d54c21ea37a3d2c01369
Gala <galinas.vm@gmail.com>
2021-02-26 18:52:10 +0200

    add npm-run-all and some other devDependences

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'no-nested-ternary': 2,
+    'object-shorthand': [2, 'always'],
+    'react/jsx-curly-brace-presence': [2, 'never'],
+  },
 };
